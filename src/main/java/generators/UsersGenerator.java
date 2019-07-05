9692cc9fd9c4fab71552dd5849c18b1fc2b8974b
Arek <arkadiusz.wielga@outlook.com>
2019-07-05 23:26:51 +0200

    Array fix

--- src/main/java/generators/UsersGenerator.java
+++ src/main/java/generators/UsersGenerator.java
@@ -5,7 +5,7 @@ import model.User;
 public class UsersGenerator {
 
     public static User[] genrateUsers(){
-        User[] users = new User[2];
+        User[] users = new User[3];
 
         for (int i = 0; i < 3; i++) {
             User user = new User(i,"Kowaslski","Karol",26);
